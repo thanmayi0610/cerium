8279ebb9239206591f08d867f3c5e3aad7f725ca
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-03-17 10:41:24 +0530

    refactor: update axios usage and improve error handling in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
-
+//import { serve } from '@hono/node-server'
+//import { Hono } from 'hono'
+/*
+import axios from "axios";
 const app = new Hono()
+const baseUrl = 'https://a90b05cb-e593-4a77-a500-82b909c678f2.mock.pstmn.io';
+try{
+  const response = axios.get(`${baseUrl}/cities`);
+  console.log(`Status`:${response.status});npm
+}
 
+  catch(e){
+    console.log("Error",e);
+  }
+
+/*
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
@@ -13,3 +24,64 @@ serve({
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
+*/
+//serve(app);
+
+/*import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+
+// serve({
+//   fetch: app.fetch,
+//   port: 3000
+// }, (info) => {
+//   console.log(Server is running on http://localhost:${info.port})
+// })
+
+// //serve(app);
+// */
+
+/*
+const data ={
+  name:"Ram",
+  age: 30
+};
+const jsonstring = JSON.stringify(data);
+console.log(jsonstring);
+
+
+const parsedData = JSON.parse(jsonstring);
+console.log(parsedData);
+*/
+import axios from "axios";
+
+const baseURl = "https://a90b05cb-e593-4a77-a500-82b909c678f2.mock.pstmn.io";
+
+
+  try {
+    const response = await axios.get(`${baseURl}/cities`);
+    
+
+    const statusCode = response.status;
+
+    const data = response.data;
+    console.log("Status:" ,statusCode);
+    console.log("Data: ",data);
+   
+    //console.log(`Status: ${response.status}`);
+  } catch (e) {
+    console.log("Error", e);
+  }
+
+
+
+
+
